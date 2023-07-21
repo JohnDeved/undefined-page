472a2a18143eaf7f2fcd62462a69b13ad7c5a169
Johann Berger <johann@objekt.stream>
2023-07-21 18:24:14 +0200

    didnt improve shit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # vite-project
+
+todo:
+1. cache distance calculation
+2. make some calculations constant?
+3. calculate letter positons, distance etc only on resize, then loop through them on draw
--- src/main.ts
+++ src/main.ts
@@ -3,80 +3,50 @@ import './style.css'
 const canvas = document.querySelector("canvas")
 if (!canvas) throw new Error("Canvas not found")
 
-// resize canvas to fit window
-// canvas.
-// canvas.height = window.innerHeight
-
 // get canvas context
 const ctx = canvas.getContext("2d")
 if (!ctx) throw new Error("Canvas context not found")
-
-// set canvas size
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-
-// on window resize, resize canvas
-window.addEventListener("resize", () => {
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
-})
 
 let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$+-*/=%\"'`!?:;,.|&<>()[]{}^#_~@\\"
 let frame = 0
 let radius = 0
 let thickness = 150
 let speed = 1
+const text = "undefined"
 
-// draw loop
-function draw(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
-  frame++
-
-  // reset radius if inside of circle is outside of canvas
-  const canvasLongside = Math.max(canvas.width, canvas.height)
-  if (radius - thickness > canvasLongside) {
-    thickness = Math.random() * 200 + 100
-    radius = 0
-  }
+// initialized later
+let canvasLongside: number = NaN
+let radiusStepSize: number = NaN
+let center = { x: 0, y: 0 }
+let letters: ReturnType<typeof getLetters> = []
 
 
-  radius += canvasLongside / 500 * speed
-  // clear canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-  const center = { x: canvas.width / 2, y: canvas.height / 2 }
-
-  // draw center text
+const getLetters = () => {
+  // center text measurements
   ctx.fillStyle = "white"
   ctx.font = "70px 'monogram', monospace"
   ctx.shadowBlur = 50
-  ctx.shadowColor = `rgba(60, 135, 255, 255)`
+  ctx.shadowColor = `rgba(60, 135, 255, 1)`
   ctx.textBaseline = "middle"
   ctx.textAlign = "center"
-  // outside line
-  const text = "undefined"
-  ctx.fillText(text, center.x, center.y)
-  // get center text bounds
   const centerTextMeasure = ctx.measureText(text)
   const centerTextWidth = centerTextMeasure.width
   const centerTextHeight = centerTextMeasure.actualBoundingBoxAscent + centerTextMeasure.actualBoundingBoxDescent
   const centerTextPos = { x: center.x - centerTextWidth / 2, y: center.y - centerTextHeight / 2 }
-  
+
+  // letters measurements
   ctx.fillStyle = "white"
   ctx.font = "30px 'monogram', monospace"
   ctx.textBaseline = "top"
   ctx.textAlign = "left"
   const letterMeasure = ctx.measureText("A")
-  // console.log(letterMeasure)
-
-  // how many letters fit on the screen
-  const pad = 0
-  const letterHeight = (letterMeasure.actualBoundingBoxAscent + letterMeasure.actualBoundingBoxDescent) * 1.5 + pad
-  const letterWidth = letterMeasure.width + pad
-  
+  const letterHeight = (letterMeasure.actualBoundingBoxAscent + letterMeasure.actualBoundingBoxDescent) * 1.5
+  const letterWidth = letterMeasure.width
   const lettersPerRow = Math.ceil(canvas.width / letterWidth)
   const lettersPerCol = Math.ceil(canvas.height / letterHeight)
 
-  // draw letters
+  const _letters = []
+
   for (let i = 0; i < lettersPerRow; i++) {
     for (let j = 0; j < lettersPerCol; j++) {
       const x = i * letterWidth
@@ -89,74 +59,108 @@ function draw(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
         y + letterHeight > centerTextPos.y &&
         y < centerTextPos.y + centerTextHeight
       ) {
-        // // draw blue line around letter
-        // ctx.strokeStyle = "blue"
-        // ctx.lineWidth = 2
-        // ctx.strokeRect(x, y, letterWidth, letterHeight)
         continue
-      
       }
 
-      // const get random letter for this position, use sin to make it change over time
-      const random = Math.sin(frame * .0001 + (i + j * 20))
-      const random2 = Math.sin(frame * .0001 + (i + j * 20) * 130)
-
-      const letter = chars[Math.abs(Math.floor(random * chars.length))] ?? " "
-      
-      const center = { x: canvas.width / 2, y: canvas.height / 2 }
-      const dist = Math.sqrt((x - center.x) ** 2 + (y - center.y) ** 2)
-      const isInside = dist < radius
-
-      ctx.fillStyle = `rgba(255, 255, 255, ${Math.max(random2 * .1, .01)})`
-      ctx.shadowBlur = 0
-      ctx.fillText(letter, i * letterWidth, j * letterHeight)
-
-      if (isInside) {
-        // create inner circle
-        if (dist < radius - thickness && dist > radius - thickness * 1.5) {
-          ctx.fillStyle = `rgba(255, 255, 255, ${Math.max(random2 * .1, .01)})`
-          ctx.fillText(letter, i * letterWidth, j * letterHeight)
-          continue
-        }
-
-        // const opacity = 1 - Math.max((radius - dist) / thickness, 0)
-        // non linear opacity quick falloff
-        const opacity = 1 - Math.max((radius - dist) / thickness, 0)
-        const r = Math.floor(255 * (random2 * .5))
-        ctx.fillStyle = `rgba(${r}, 135, 245, ${opacity})`
-        ctx.fillText(letter, i * letterWidth, j * letterHeight)
-
-
-
-        if (dist > radius - 25) {
-          for (let k = 0; k < 5; k++) {
-            ctx.shadowBlur = 40
-            ctx.shadowColor = `rgba(60, 135, 255, 255)`
-            ctx.fillStyle = `rgba(255, 255, 255, 1)`
-            ctx.fillText(letter, i * letterWidth, j * letterHeight)
-
-          }
-        }
-
-      } 
+      _letters.push({
+        x, y, i, j,
+        dist: Math.sqrt((x - center.x) ** 2 + (y - center.y) ** 2)
+      })
     }
   }
-  // // draw line around center text
-  // ctx.strokeStyle = "white"
-  // ctx.lineWidth = 2
-  // ctx.strokeRect(centerTextPos.x, centerTextPos.y, centerTextWidth, centerTextHeight)
+
+  return _letters
+}
+
+// set runtime variables
+const setVars = () => {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  canvasLongside = Math.max(canvas.width, canvas.height)
+  radiusStepSize = canvasLongside / 500 * speed
+  center = { x: canvas.width / 2, y: canvas.height / 2 }
+  letters = getLetters()
+}
+setVars()
+
+// on window resize, reset runtime variables
+window.addEventListener("resize", setVars)
 
 
-  // // draw blue dot at center text position
-  // ctx.fillStyle = "blue"
-  // ctx.beginPath()
-  // ctx.arc(centerTextPos.x, centerTextPos.y, 5, 0, Math.PI * 2)
-  // ctx.fill()
+// draw loop
+const draw = () => {
+  frame++
 
+  // reset radius if inside of circle is outside of canvas
+  if (radius - thickness > canvasLongside) {
+    thickness = Math.random() * 200 + 100
+    radius = 0
+  }
+
+  // increase radius
+  radius += radiusStepSize
+
+  // clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  // draw center text
+  ctx.fillStyle = "white"
+  ctx.font = "70px 'monogram', monospace"
+  ctx.shadowBlur = 50
+  ctx.shadowColor = `rgba(60, 135, 255, 1)`
+  ctx.textBaseline = "middle"
+  ctx.textAlign = "center"
+  ctx.fillText(text, center.x, center.y)
+
+  // draw letters
+  ctx.fillStyle = "white"
+  ctx.font = "30px 'monogram', monospace"
+  ctx.textBaseline = "top"
+  ctx.textAlign = "left"
+
+  for (let i = 0; i < letters.length; i++) {
+    const letterData = letters[i]
+    // const get random letter for this position, use sin to make it change over time
+
+    const random = Math.sin(frame * .0001 + (letterData.i + letterData.j * 20))
+    const random2 = Math.sin(frame * .0001 + (letterData.i + letterData.j * 20) * 130)
+
+    const letter = chars[Math.abs(Math.floor(random * (chars.length - 1)))] ?? " "
+    const isInside = letterData.dist < radius
+
+    ctx.fillStyle = `rgba(255, 255, 255, ${Math.max(random2 * .1, .01)})`
+    ctx.shadowBlur = 0
+    ctx.fillText(letter, letterData.x, letterData.y)
+
+    if (isInside) {
+      // create inner circle
+      if (letterData.dist < radius - thickness && letterData.dist > radius - thickness * 1.5) {
+        ctx.fillStyle = `rgba(255, 255, 255, ${Math.max(random2 * .1, .01)})`
+        ctx.fillText(letter, letterData.x, letterData.y)
+        continue
+      }
+
+      // const opacity = 1 - Math.max((radius - dist) / thickness, 0)
+      // non linear opacity quick falloff
+      const opacity = 1 - Math.max((radius - letterData.dist) / thickness, 0)
+      const r = Math.floor(255 * (random2 * .5))
+      ctx.fillStyle = `rgba(${r}, 135, 245, ${opacity})`
+      ctx.fillText(letter, letterData.x, letterData.y)
+
+      if (letterData.dist > radius - 25) {
+        for (let k = 0; k < 5; k++) {
+          ctx.shadowBlur = 40
+          ctx.shadowColor = `rgba(60, 135, 255, 1)`
+          ctx.fillStyle = `rgba(255, 255, 255, 1)`
+          ctx.fillText(letter, letterData.x, letterData.y)
+        }
+      }
+    }
+  }
 
   // request next frame
-  requestAnimationFrame(draw.bind(null, canvas, ctx))
+  requestAnimationFrame(draw)
 }
 
 // start draw loop
-draw(canvas, ctx)
+draw()
